3e067088dd2220910ba22940605e32c94208b0d5
elvispy <elvisavfc65@gmail.com>
2020-04-21 10:05:21 -0400

    Added comment

--- flask_api/geojson_api.py
+++ flask_api/geojson_api.py
@@ -31,6 +31,7 @@ COMPRESS_MIMETYPES = [
 COMPRESS_LEVEL = 6
 COMPRESS_MIN_SIZE = 500
 
+#Ruta del archivo geojson en el SO
 GEOJSON_PATH = str(pathlib.Path(__file__).parent.absolute() / "geojson_data/paraguay_2012_barrrios_y_localidades.geojson")
 
 tekopora_key = os.getenv("TEKOPORA")
