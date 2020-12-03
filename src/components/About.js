87bdc65de742c61adc6201823930cca7aaaee246
alesservin <alejandro.sebastian.servin@gmail.com>
2020-12-03 15:24:11 -0300

    Add hamburger menu, modify css and add About page

---
+++ src/components/About.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../css/about.css';
+
+export default function About() {
+  return (
+    <>
+      <div className="about-content">
+        <center>
+          <h1> Acerca del Priorizador </h1>
+        </center>
+        <p>
+          El Priorizador es una propuesta de mecanismo de focalización para las
+          transferencias monetarias de la SEN y entregas de kits alimenticios
+          COVID-19.
+        </p>
+        <p>Pr&oacute;ximamente m&aacute;s informaci&oacute;n...</p>
+
+        <center>
+          <h1>¿De d&oacute;nde provienen los datos?</h1>
+        </center>
+        <p>Pr&oacute;ximamente m&aacute;s informaci&oacute;n...</p>
+
+        <center>
+          <h1>¿C&oacute;mo interpretar la informaci&oacute;n?</h1>
+        </center>
+        <p>Pr&oacute;ximamente m&aacute;s informaci&oacute;n...</p>
+
+        <p>
+          M&aacute;s informaci&oacute;n&nbsp;
+          <a
+            href="https://d-riveros-garcia.medium.com/una-propuesta-para-que-la-ayuda-covid-19-llegue-a-tantas-familias-paraguayas-como-sea-posible-8adfe1101806"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            aqu&iacute;
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
